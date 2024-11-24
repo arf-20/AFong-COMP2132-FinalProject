89b064a43cc0f85b086f2276ed59fa70b64d0258
arf-20 <andrewfong630@hotmail.com>
2024-11-24 01:33:16 -0800

    Update final-project.js

--- js/final-project.js
+++ js/final-project.js
@@ -23,3 +23,38 @@ let compScore = 0;
 let compTotal = 0;
 let winner = '';
 let round = 0;
+
+class Dice {
+    constructor(color){
+        this.sides = [1, 2, 3, 4, 5, 6];
+        this.currentSide = 0;
+        this.color = color;
+    }
+
+    describeSelf(){
+        return `<img src="images/${this.color}_${this.currentSide}.png" alt="${this.color} Dice. Value: ${this.currentSide}">`;
+    }
+}
+
+Dice.prototype.roll = function(){
+    let result = Math.round(Math.random() * (this.sides.length - 1));
+
+    this.currentSide = this.sides[result];
+
+    return this.currentSide;
+}
+
+//Function to calculate the score when two dice are rolled
+function calculateScore(diceVal1, diceVal2){
+    let score;
+
+    if(diceVal1 === 1 || diceVal2 === 1){
+        score = 0;
+    }else if(diceVal1 === diceVal2){
+        score = (diceVal1 + diceVal2) * 2;
+    }else{
+        score = diceVal1 + diceVal2;
+    }
+
+    return score;
+}
